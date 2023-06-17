06e115480ac7413e7c5a4e5ae1169d7debd238b2
Shimei <shimeilauraleeb@yahoo.com>
2023-06-16 20:18:43 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
 		if (n != 57)
 	}
-			putchar('_');
+		{
+			putchar(',');
 			putchar(' ');
-	}
+		}
 	putchar('\n');
 	return (0);
 }
