b0cf7735b1a401f41a3fdde8c79befcbc01c7db8
Shimei <shimeilauraleeb@yahoo.com>
2023-06-20 16:21:43 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -2,13 +2,14 @@
 
 
 /**
-* * main - check the code
-*  *
-*   * Return: Always 0.
-*    */
+* main - check the code
+*
+* Return: Always 0.
+*/
+
 int main(void)
 {
-	
-    jack_bauer();
-    return (0);
+
+jack_bauer();
+return (0);
 }
