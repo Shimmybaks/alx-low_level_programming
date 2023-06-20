5116b1a77bce5ff465fd38f939e831bda2e13084
Shimei <shimeilauraleeb@yahoo.com>
2023-06-20 12:39:05 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,25 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
-
-void print_square(int size)
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-int i, j;
-
-if (size > 0)
-{
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-printf("#");
-}
-printf("\n");
-}
-}
-else
-{
-printf("\n");
-
-}
+	jack_bauer();
+	return (0);
 }
