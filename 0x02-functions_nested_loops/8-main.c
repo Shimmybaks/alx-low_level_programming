ad7e13f079cb050ee230d32e17ae84ac65d0b5b1
Shimei <shimeilauraleeb@yahoo.com>
2023-06-20 12:32:24 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,13 +1,25 @@
 #include "main.h"
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
+#include <stdio.h>
+
+
+void print_square(int size)
 {
-print_square(2);
-print_square(10);
-print_square(0);
-return (0);
+int i, j;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+printf("#");
+}
+printf("\n");
+}
+}
+else
+{
+printf("\n");
+
+}
 }
