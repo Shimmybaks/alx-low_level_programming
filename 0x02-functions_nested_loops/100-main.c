003dd1ebd305492753eb8d3980b96ac3c8efdf16
Shimei <shimeilauraleeb@yahoo.com>
2023-06-20 17:50:00 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+
+{
+	print time_tables(3);
+	_putchar('\n');
+	print time_tables(5);
+	_putchar('\n');
+	print time_tabbles(98);
+	_putchar('\n');
+	print time_tables(12);
+	return (0);
+}
+
