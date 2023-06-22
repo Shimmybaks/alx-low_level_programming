dacd159e75769857bd4a3bee384dbe3257f57592
Shimei <shimeilauraleeb@yahoo.com>
2023-06-22 12:22:14 -0700

    Debugging

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,21 +5,21 @@
 */
 void print_diagonal(int n)
 {
+int x, y;
+
 if (n <= 0)
 {
 _putchar('\n');
-} else
-{
-int i, j;
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < n; j++)
-{
-if (j == i)
-_putchar('\\');
-else if (j < i)
-_putchar(' ');
 }
+else
+{
+for (x = 0; x < n; x++)
+{
+for (y = 0; y < n; y++)
+{
+_putchar(32);
+}
+_putchar(92);
 _putchar('\n');
 }
 }
