88ccbe2c8a998760e73c3f3cb0e49811429f8be6
Shimei <shimeilauraleeb@yahoo.com>
2023-07-07 14:08:43 -0700

    0x09-static_libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- *_puts - function that prints a string, followed by a new line, to stdout
- *
- * @str: parameter defined in main 
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-
-void puts(char *str)
+void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str);
-		str++;
+		_putchar(*str++);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
